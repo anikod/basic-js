a744f99974b1e1b9472bde63b8d5faeb2a1e7c1d
anikod <nevelikobo@gmail.com>
2021-03-24 21:41:07 +0300

    feat: count-cats finished

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,27 @@
-const CustomError = require("../extensions/custom-error");
+//const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats(matrix) {
+//function countCats(matrix) {
+  //throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  let result = 0;
+  for (let index = 0; index < matrix.length; index++) {
+    //const element = array[index];
+    //console.log('index: ', index);
+
+    for (let m = 0; m < matrix[index].length; m++) {
+      //const element = array[m];
+      //console.log('m is: ', matrix[index][m]);
+      if (matrix[index][m] == '^^') result++;
+    }
+
+  }
+
+
+
+
+
+
+  return result;
 };
+
